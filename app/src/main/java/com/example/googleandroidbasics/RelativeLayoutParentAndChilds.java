8d048c9e3205084e7c3f925f8ccc80b07b45d772
DennsHartstock <dennis.hartstock@web.de>
2021-11-28 09:38:57 +0100

    add a RelativeLayout with a Parent and his children

---
+++ app/src/main/java/com/example/googleandroidbasics/RelativeLayoutParentAndChilds.java
@@ -0,0 +1,14 @@
+package com.example.googleandroidbasics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayoutParentAndChilds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_parent_and_childs);
+    }
+}

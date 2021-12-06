350532cb240029addc9346266dc422c5578f0b4c
DennsHartstock <dennis.hartstock@web.de>
2021-12-06 11:22:50 +0100

    add increment and decrement

--- app/src/main/java/com/example/googleandroidbasics/JustJava.java
+++ app/src/main/java/com/example/googleandroidbasics/JustJava.java
@@ -1,14 +1,68 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
 package com.example.googleandroidbasics;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.NumberFormat;
 
+/**
+ * This app displays an order form to order coffee.
+ */
 public class JustJava extends AppCompatActivity {
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_just_java);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        if (quantity < 0)
+            quantity = 0;
+        else
+            quantity--;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
